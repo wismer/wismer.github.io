3bc38beeef655664516fdaa7abf8d7152a491056
Matt Long <matthewhl@gmail.com>
2024-08-10 17:10:03 -0400

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,15 +15,6 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
           </a>
         </div>
       </div>
