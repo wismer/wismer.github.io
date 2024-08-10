2944365f5a8291a729dba1efb2574952ed58a7ae
Matt Long <matthewhl@gmail.com>
2024-08-10 17:16:18 -0400

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
+	images: {
+		unoptimized: true,
+	},
+	basePath: '/wismer.github.io',
 };
 
 export default nextConfig;
